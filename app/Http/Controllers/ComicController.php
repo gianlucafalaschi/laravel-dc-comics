dae42f0b8beaedff5a1156cfc1ac117f53952c92
gianlucafalaschi <gian100gian@outlook.it>
2024-05-23 23:08:17 +0200

    function store and redirect

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -43,6 +43,19 @@ class ComicController extends Controller
     {
         $formData = $request->all();
         
+        // Creare nuova riga nel database
+        $newComic = new Comic();
+        $newComic->title = $formData['title'];
+        $newComic->description = $formData['description'];
+        $newComic->thumb = $formData['thumb'];
+        $newComic->price = $formData['price'];
+        $newComic->series = $formData['series'];
+        $newComic->sale_date = $formData['sale_date'];
+        $newComic->type = $formData['type'];
+        $newComic->save();
+        
+        // quando l'utente crea un nuovo comic, lo reindirizzo alla pagina del singolo comic aggiunto
+        return redirect()->route('comics.show', ['comic' => $newComic->id]);
     }
 
     /**
