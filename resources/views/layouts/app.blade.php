6e07f3f3bcd47b241f60b2f5a7afc8d2d549e334
gianlucafalaschi <gian100gian@outlook.it>
2024-05-25 17:40:08 +0200

    basic navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,28 @@
 </head>
 
 <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="{{ route('home') }}">COMICS</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="{{ route('comics.index') }}">Lista prodotti</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('comics.create') }}">Crea nuovo prodotto</a>
+                </li>
+              </ul>
+              
+            </div>
+        </nav>   
+    </header>
 
-    <main class="bg-light">
+    <main class="">
         @yield('content')
     </main>
 
