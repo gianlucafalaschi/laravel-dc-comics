787c33e85806bd1776afee956aaba0ba19c662c0
gianlucafalaschi <gian100gian@outlook.it>
2024-05-27 09:56:18 +0200

    description removed for better visual layout

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,11 +7,11 @@
             <div class="row">
                 @foreach ($comics as $comic)
                 <div class="col">
-                    <div class="card" style="width: 18rem;">
+                    <div class="card my-3" style="width: 18rem;">
                         <img class="card-img-top" src="https://picsum.photos/id/237/200/300" alt="Card image cap">
                         <div class="card-body">
                           <h5 class="card-title">{{$comic->title}}</h5>
-                          <p class="card-text">{{$comic->description}}</p>
+                          {{-- <p class="card-text">{{$comic->description}}</p> --}}
                         </div>
                         <ul class="list-group list-group-flush">
                           <li class="list-group-item">{{$comic->price}} euro</li>  {{-- essendo  l'istanza di un oggetto devo usare la freccina, se fosse un array associativo sarebbe {{$comic['price']}}   --}}
