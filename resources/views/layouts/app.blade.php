94b16de8a8d710dc0979828ac6458ba67753b041
gianlucafalaschi <gian100gian@outlook.it>
2024-05-27 15:16:59 +0200

    navbar toggler fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,24 +17,24 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="{{ route('home') }}">COMICS</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="{{ route('comics.index') }}">Lista prodotti</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('comics.create') }}">Crea nuovo prodotto</a>
-                </li>
-              </ul>
-              
-            </div>
-        </nav>   
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="{{ route('home') }}">COMICS</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('comics.index') }}">Lista prodotti</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('comics.create') }}">Crea nuovo prodotto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>   
     </header>
 
     <main class="">
